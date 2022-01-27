a841ac2c7ab390851978406e05ccce1eee54552c
Alapi <59810553+alapishah@users.noreply.github.com>
2022-01-27 17:45:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_M
 	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
 	maxZoom: 16
 }).addTo(mymap);
-
-var marker = L.marker([37.7749, -122.4194]).addTo(mymap);
-
 $.getJSON('https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson', function(data) {
   var coordinatesOnly = data.features.map(function(feature) {
     return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];});
+var marker = L.marker([37.7749, -122.4194]).addTo(mymap);
+
+
 marker.bindPopup("<b>Saint Louis!</b><br>I've Got Great Food").openPopup();
 
 
