2fd0b05125a68858bca5d88f9e255dae7636e9d6
Alapi <59810553+alapishah@users.noreply.github.com>
2022-01-30 13:26:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ ext: 'png'
 
   // load GeoJSON from an external file
   $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
-         var thiefIcon = L.icon({
+         var ratIcon = L.icon({
     iconUrl: 'https://e7.pngegg.com/pngimages/170/541/png-clipart-robbery-burglary-crime-theft-thief-mammal-people.png',
     iconSize: [30,30]
   }); 
   L.geoJson(data  ,{
     pointToLayer: function(feature,latlng){
-	    return L.marker(latlng,{icon: thiefIcon});
+	    return L.marker(latlng,{icon: ratIcon});
     },
     onEachFeature: function (feature, layer) {
       layer.bindPopup('<h1>Title: '+feature.properties.title+'</h1><p>Description:  '+feature.properties.description+'</p>');
